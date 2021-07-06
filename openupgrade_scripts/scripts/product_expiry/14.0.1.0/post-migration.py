af748ce1e6c2388f258d7429bd968a6dc386ca4e
mreficent <miquel.raich@eficent.com>
2021-07-06 16:08:47 +0200

    [MIG] product_expiry (continuation)

--- openupgrade_scripts/scripts/product_expiry/14.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/product_expiry/14.0.1.0/post-migration.py
@@ -7,11 +7,21 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     openupgrade.logged_query(
         env.cr,
-        """ UPDATE
-                product_template
-            SET
-                use_expiration_date = 't'
-            WHERE
-                expiration_time > 0
         """
+        UPDATE product_template
+        SET use_expiration_date = TRUE
+        WHERE expiration_time > 0
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE stock_move_line sml
+        SET expiration_date = spl.expiration_date
+        FROM stock_production_lot spl, product_product pp
+        JOIN product_template pt ON pp.product_tmpl_id = pt.id, stock_picking sp
+        JOIN stock_picking_type spt ON sp.picking_type_id = spt.id
+        WHERE spl.id = sml.lot_id AND sml.picking_id = sp.id
+            AND spt.use_existing_lots AND sml.product_id = pp.id
+            AND pt.use_expiration_date""",
     )
